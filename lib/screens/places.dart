10b09ed77f9ace44abb3032aa6c35c7d63e562bb
praty474 <p.kayastha.pk@gmail.com>
2024-02-20 23:54:51 +0545

    provider setup 6

---
+++ lib/screens/places.dart
@@ -0,0 +1,26 @@
+import 'package:favorite_app/screens/add_place.dart';
+import 'package:favorite_app/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()));
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(places: []),
+    );
+  }
+}
