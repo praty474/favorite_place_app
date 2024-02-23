cff659043038e2c498c0bc135d1e685bd9a97b1d
praty474 <p.kayastha.pk@gmail.com>
2024-02-23 22:47:57 +0545

    location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:favorite_app/models/place.dart';
 import 'package:favorite_app/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
